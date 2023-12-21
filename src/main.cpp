12d0bdb5ab8aa874c76fea53b838e152340bac40
Aidan-B <abowers@uwaterloo.ca>
2023-12-21 11:42:15 -0500

    Use new headset interface in main
    - Move libusb initialization to main
    - Request a battery packet, then print it immediately

--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,114 @@
-#include "ArctisHeadset.h"
+#include "NewHeadset.h"
 
 #include <libusb-1.0/libusb.h>
 #include <iostream>
 
+void print_packet(const Packet *packet) {
+    const uint8_t* buf = reinterpret_cast<const uint8_t*>(packet);
+    for (long unsigned int i = 0; i < sizeof(*packet); i++) {
+        printf("%02x ", buf[i]);
+    }
+    printf("\n");
+}
+
 int main() {
 
-	libusb_context *ctx = NULL; //a libusb session
-	int ret; //for return values
+	libusb_context* ctx = nullptr; //a libusb session
+    libusb_device** device_list = nullptr;
+    libusb_device* device = nullptr;
+	libusb_device_handle* handle = nullptr;
+	bool detachedKernelDriver = false;
+
+    int err = 0;
 	
-	//setup
-	ret = libusb_init(&ctx); //initialize the library for the session we just declared
-	if(ret != LIBUSB_SUCCESS) {
-		std::cout << libusb_strerror(ret) << std::endl;
-		return 1;
+
+	// Initialize the library
+	err = libusb_init(&ctx);
+	
+	// TODO: This error handling just exits when anything bad happens. 
+	//		 It should be a little more robust. (Get rid of try-throw-catch)
+	try {
+		if (err != LIBUSB_SUCCESS) {
+			throw libusb_error(err);
+		}
+		
+		// libusb_set_option(ctx, LIBUSB_OPTION_LOG_LEVEL, LIBUSB_LOG_LEVEL_DEBUG);
+		
+		// Get the list of devices, then select the correct one from the list
+		ssize_t size = libusb_get_device_list(ctx, &device_list);
+		if (size < 0) {
+			throw libusb_error(size);
+		}
+		for (ssize_t i = 0; i < size; i++) {
+			libusb_device_descriptor desc;
+			err = libusb_get_device_descriptor(device_list[i], &desc);
+			if (err != LIBUSB_SUCCESS) {
+				throw libusb_error(err);
+			}
+			if (desc.idVendor == 0x1038 &&      // SteelSeries ApS
+				desc.idProduct == 0x12ad &&
+				desc.iManufacturer == 4 &&      // SteelSeries
+				desc.iProduct == 5)             // SteelSeries Arctis 7
+			{
+				device = device_list[i];
+				break;
+			}
+		}
+		if (device == nullptr) {
+			throw libusb_error(LIBUSB_ERROR_NOT_FOUND);
+		}
+
+		// Open the headset with libusb and claim the interface
+		err = libusb_open(device, &handle);
+		if (err != LIBUSB_SUCCESS) {
+			throw libusb_error(err);
+		}
+
+		libusb_free_device_list(device_list, true);
+		device = nullptr;
+
+		if (libusb_has_capability(LIBUSB_CAP_SUPPORTS_DETACH_KERNEL_DRIVER) && 
+			libusb_kernel_driver_active(handle, Headset::interface))
+		{    
+			detachedKernelDriver = true;
+			err = libusb_detach_kernel_driver(handle, Headset::interface);
+			if (err != LIBUSB_SUCCESS) {
+				libusb_close(handle);
+				
+				throw libusb_error(err);
+			}
+		}
+
+		err = libusb_claim_interface(handle, Headset::interface);
+		if (err != LIBUSB_SUCCESS) {
+			if (detachedKernelDriver) {
+				libusb_attach_kernel_driver(handle, Headset::interface);
+			}
+			libusb_close(handle);
+			throw libusb_error(err);
+		}
+
+
+		Headset headset(handle);
+
+		Battery request;
+		Packet response{.command=0};
+		
+		headset.read(&request, &response);
+		print_packet(&response);
+
+
+		if (detachedKernelDriver) {
+			libusb_attach_kernel_driver(handle, Headset::interface);
+		}
+		libusb_close(handle);
+
 	}
-	// libusb_set_option(ctx, LIBUSB_OPTION_LOG_LEVEL, LIBUSB_LOG_LEVEL_DEBUG);
-	libusb_set_option(ctx, LIBUSB_OPTION_LOG_LEVEL, LIBUSB_LOG_LEVEL_INFO);
-	try
-	{	
-		Arctis7Headset headset(ctx);
-		
-		// unsigned char req_one[31] = {0x06, 0x14, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
-		// unsigned char req_two[31] = {0x06, 0x18, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
-		
-		std::string input;
-		do {
-			// headset.FeatureRequest(req_one, 31);
-			// headset.FeatureRequest(req_two, 31);
-		} while (getline( std::cin, input ) && input.empty());
-		
-	}
-	catch(const std::exception& e) {
-		std::cerr << "Exception occured: " << e.what() << "\n";
-	} catch(const libusb_error& e) {
-		std::cerr << "Exception occured: " << libusb_strerror(e) << std::endl;
-	} catch(const libusb_transfer_status& e) {
-		std::cerr << "Exception occured: " << libusb_error_name(e) << std::endl;
-	} catch (...) {
-		std::cerr <<  "An unknown exception has occurred" << std::endl;
+	catch(const libusb_error& e) {
+		std::cerr << "Error: " << libusb_strerror(e) << std::endl;
 	}
 
-	libusb_exit(ctx); //needs to be called to end the
+	libusb_exit(ctx);
 
 	return 0;
 }
